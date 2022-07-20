00c750b0167bc9d5b9715f778d2b70a19f9b7000
yojith712 <109545439+yojith712@users.noreply.github.com>
2022-07-19 19:22:35 -0500

    Create program 5 6.py

---
+++ program 5 6.py
@@ -0,0 +1,11 @@
+print('Find bill, discount and net sales')
+print('Enter quantity')
+a=int(input())
+print('Enter rate of an item')
+b=float(input())
+c=a*b
+print('Total bill',c)
+d=(20/100)*c
+print('Discont',d)
+e=c-d
+print('net sales',e)
