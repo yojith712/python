4866bc816f5ded9f01c22f3af6b8e61ad5428bf6
yojith712 <109545439+yojith712@users.noreply.github.com>
2022-07-22 13:51:29 -0500

    Create progress.c

---
+++ progress.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+float number1;
+float number2;
+float answer;
+int choice;
+printf("please type in the beside opreater")
+printf("/n/t1)addiation");
+printf("/n/t2)subbtraction");
+printf("/n/t3/divison");
+printf("/n/t3/multiplaction");
+printf("what is your choice");
