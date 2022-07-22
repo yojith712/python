10fe980a8b987e3726bf3b265d31aab85a79c341
yojith712 <109545439+yojith712@users.noreply.github.com>
2022-07-22 17:34:19 -0500

    Create program 24.py

---
+++ program 24.py
@@ -0,0 +1,10 @@
+print(' three digit ')
+a=int(input())
+firstnumber=a//100
+reminder=a%100
+print('firstnumber',firstnumber)
+print('reminder',reminder)
+secondnumber=reminder//10
+thirdnumber=reminder%10
+sum=firstnumber+secondnumber+thirdnumber
+print('three digit sum',sum)
