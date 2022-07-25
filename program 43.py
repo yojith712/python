4277f6c909c7188b76f250f0355186fc08caa736
yojith712 <109545439+yojith712@users.noreply.github.com>
2022-07-25 12:49:02 -0500

    Create program 43.py

---
+++ program 43.py
@@ -0,0 +1,7 @@
+print('enter employee basic salary')
+salaryamount=float(input())
+increment=(30/100)*salaryamount
+print('increment',increment)
+incrementsalary=salaryamount+increment
+print('incrementsalary',incrementsalary)
+
