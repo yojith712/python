b6e36db9be4fd5f1ce4bf473f6ad5e6fb9579e9b
yojith712 <109545439+yojith712@users.noreply.github.com>
2022-07-27 09:29:23 -0500

    Create program 010.py

---
+++ program 010.py
@@ -0,0 +1,11 @@
+print('enter 2 numbers')
+a=int(input())
+b=int(input())
+addiation=a+b
+print('addiation',addiation)
+subtraction=a-b
+print('subtraction',subtraction)
+multiplaction=a*b
+print('multiplaction',multiplaction)
+divsison=a/b
+print('divsison',divsison)
