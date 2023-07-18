60c543bc82028ea978ab06588c080c35b4ee7936
yojith712 <109545439+yojith712@users.noreply.github.com>
2023-07-18 18:10:04 -0500

    Create 139program.py

---
+++ 139program.py
@@ -0,0 +1,11 @@
+#accept a number and print the factors of given number
+
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = 320
+
+print_factors(num)
