56d75e0b9f3458dc936b33b463a0f9b46295ce4a
yojith712 <109545439+yojith712@users.noreply.github.com>
2023-07-18 23:01:36 -0500

    Create 145program.py

---
+++ 145program.py
@@ -0,0 +1,11 @@
+#accept a number t find and print the reverse of the given number
+ 
+def reverse(n):
+     
+     rev=0
+     while n>0:
+         rev=rev*10+n%10
+         n=n//10
+     return rev 
+n=int(input('enter n value '))
+print(reverse(n))
