217a21c39d0c317b08ecb7fb546149f8c971ac44
yojith712 <109545439+yojith712@users.noreply.github.com>
2023-07-25 13:54:44 -0500

    Create programabcpattern4.py

---
+++ programabcpattern4.py
@@ -0,0 +1,9 @@
+# Python Program to print number pattern using for l
+def pattern(n):
+    alphabet=["A","B","C","D","E","F","G","H","I","J"]
+    for i in range(n):
+      for j in range(n):
+         print(alphabet[i],end="")
+      print("\r")
+n=int(input("enter a number :"))
+pattern(n)
