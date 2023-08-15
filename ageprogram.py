ce371ad9b2060f0dedf6e2b8b7965bec1ce239b7
yojith712 <109545439+yojith712@users.noreply.github.com>
2023-08-15 13:06:15 -0500

    Create ageprogram.py

---
+++ ageprogram.py
@@ -0,0 +1,12 @@
+print('enter age')
+n=int(input())
+if n>=0 and n<=10:
+     print('jay')
+
+elif n>=10 and n <= 12 :
+     print('yojith')
+     
+elif n>=13 and n<=100:
+     print('ayyappa')
+else:
+    print('no person')
